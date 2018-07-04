80f628807bbc482b45da613a0bf366a617b98c7d
James Kamana <jaykamana@gmail.com>
2018-07-04 21:12:05 +0300

    Setup client with reactstrap

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose
 
 app.use('/api/items', items);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`app running on port ${port}`);
